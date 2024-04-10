140aeaf1f8608a434b48b47466a2e981fed06535
SaurabPoudel <poudelsaurab20@gmail.com>
2024-04-10 17:52:38 +0545

    stream actions meeting setup and providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+// will load the default styling for the React Video SDK
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,7 +35,9 @@ export default function RootLayout({
       }}
     >
       <html lang="en">
-        <body className={`${inter.className} bg-dark-2`}>{children}</body>
+        <body className={`${inter.className} bg-dark-2`}>
+          {children} <Toaster />
+        </body>
       </html>
     </ClerkProvider>
   );
